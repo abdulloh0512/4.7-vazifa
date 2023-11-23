96f270411f69c568bff7581c9a2ae38ba8b56d31
abdulloh0512 <abdullohdev0512@gmail.com>
2023-11-24 01:24:44 +0500

    4.7

---
+++ script.js
@@ -0,0 +1,295 @@
+// 15-masala
+// function noldan(a) {
+//     if (a <= 0) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+
+// noldan(+prompt("Hohlagan biror son kiriting."))
+
+// 16-masala
+// function burchak(a) {
+//     console.log(
+//         (a-2)*180
+//     );
+// }
+
+// burchak(+prompt("Ucburchak nechta burchakligini kiritng."))
+
+// 17-masala
+// function achko(a, b) {
+//   console.log(a * 2 + b * 3);
+// }
+
+// achko(
+//   +prompt("2 achkoli gollar sonini kiriting."),
+//   +prompt("3 achkolik gollar sonini kiriting.")
+// );
+
+// 18-masala
+// function edabit(a) {
+//     console.log(String(a) + "Edabit");
+// }
+
+// edabit(prompt("Hohlagan sozingizni kiriting."))
+
+// 19-masala
+// function yigindi(a, b) {
+//     if (a + b < 100) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+
+// yigindi(+prompt("Birinchi sonni kiriting."), +prompt("Ikkinchi sonni kiriting."))
+
+// 20-masala
+// function printArray(a) {
+//     console.log(Array(a));
+// }
+
+// printArray(+prompt("Biror son kiriting."))
+
+// 21-masala
+// function oyoqlar(tovuq, qoy, sigir) {
+//   console.log(`Barcha oyoqlar soni ${tovuq * 2 + qoy * 4 + sigir * 4} ta`);
+// }
+
+// oyoqlar(
+//   +prompt("Tovuqlar sonini kiriting."),
+//   +prompt("Qo'ylar sonini kiriting."),
+//   +prompt("Sigirlar sonini kiriting.")
+// );
+
+// 22-masala
+// function and(a, b) {
+//     console.log(a > b && a < b);
+// }
+
+// and(+prompt("Birinchi sonni kiriting."), +prompt("Ikkinchi sonni kiriting."))
+
+// 23-masala
+// function tengMi(a, b) {
+//     if (a == b) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+
+// tengMi(+prompt("Birinchi sonni kiriting."), +prompt("Ikkinchi sonni kiriting."))
+
+// 24-masala
+// function achko(galaba, durang, maglubiyat) {
+//   console.log(
+//     `Umumiy achkolar soni ${galaba * 3 + durang * 1 + maglubiyat * 0} achko`
+//   );
+// }
+
+// achko(
+//   +prompt("G'alabalalar sonini kiriting."),
+//   +prompt("duranglar sonini kiriting."),
+//   +prompt("Mag'lubiyatlar sonini kiriting.")
+// );
+
+// 25-masala
+// function toSecund(soat, minut) {
+//   console.log(`${soat * 60 * 60 + minut * 60} secund`);
+// }
+
+// toSecund(
+//   +prompt("Soatni kiriting agar soat bolmasa 0 ni kiriting."),
+//   +prompt("Minutlarni kiriting.")
+// );
+
+// 26-masala
+// function tub(son) {
+//     console.log("Bu masalaga chunmadim .");
+// }
+
+// tub()
+
+// 27-masala
+// function chekEquality(a, b) {
+//     if (a == b) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+
+// chekEquality(prompt("Biror narsa yozing"), prompt("Yana biror narsa yozing"))
+
+// 28-masala
+// function booleanToString(a) {
+//     console.log(a);
+//     let c = String(a);
+// }
+
+// booleanToString(Boolean(prompt("Biror narsa kiriting.")));
+
+// 29-masala
+// let a = prompt("")
+//  let arrowFunc = (a) => {
+//     console.log(a);
+// }
+
+// arrowFunc(prompt("Hohlagan maumot yozing"))
+
+// 31-masala
+// function calculateMathString(mathString) {
+//     return eval(mathString);
+//   }
+
+//   console.log(calculateMathString("4 + 3 * 2"));
+
+// 32-masala
+// function teng10(a, b) {
+//   if (a == 10 || b == 10 || a + b == 10) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+
+// teng10(
+//   +prompt("Birinchi sonni kiriting."),
+//   +prompt("Ikkinchi sonni kiriting.")
+// );
+
+// 33-masala
+// function benzin(m) {
+//   console.log(m / 10);
+// }
+
+// benzin(+prompt("Necha kilometerligini kiriting."));
+
+// 34-masala
+// function arr(a, b) {
+//   if (a > b) {
+//     console.log(a);
+//   } else if (a < b) {
+//     console.log(b);
+//   } else {
+//     console.log("Ikkalasi teng");
+//   }
+// }
+
+// arr(+prompt("birinchi sonni kiriting."), +prompt("Ikkinchi sonni kiriting."));
+
+// 35-masala
+// function arr(a, b) {
+//     console.log(Array(a, b));
+// }
+
+// arr(+prompt("birinchi sonni kiriting."), +prompt("Ikkinchi sonni kiriting."));
+
+// 36-masala
+// function tengString(a, b) {
+//   if (a.length == b.length) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+
+// tengString(
+//   prompt("Birinchi sozni kiriting."),
+//   prompt("Ikkinchi sozni kiriting.")
+// );
+
+// 37-masala
+// function boshString(a) {
+//   if (a.length == 0) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+
+// boshString(prompt("biror soz kiriting."));
+
+// 38-masala
+// function bolish(a) {
+//   if (a % 5 == 0) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+
+// bolish(+prompt("biror butun son kiriting."));
+
+// 39-masala
+// function bolish(a) {
+//   if (a % 100 == 0) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+
+// bolish(+prompt("biror butun son kiriting."));
+
+// 40-masala
+// function uzunlik(a) {
+//   console.log(a.length);
+// }
+
+// uzunlik(prompt("Biror soz kiriting."));
+
+// 41-masala
+// function bolinish(a, b) {
+//   if (a < b) {
+//     if (b % a == 0) {
+//       console.log(true);
+//     } else {
+//       console.log(false);
+//     }
+//   } else {
+//     console.log("1- son ikkinchisidan kichik emas.");
+//   }
+// }
+
+// bolinish(
+//   +prompt("Birinchi sonnni kiriting."),
+//   +prompt("Ikkinchi sonni kiriting.")
+// );
+
+// 42-masala
+// function raqam(a) {
+//     return console.log(Number(a));
+// }
+
+// raqam(prompt("biror son kiriting."))
+
+// 43-masala
+// function tortburchak(a, b, c, d) {
+//   if (a == c || b == d) {
+//     console.log(a * b);
+//   } else {
+//     console.log(-1);
+//   }
+// }
+
+// tortburchak(
+//   +prompt("Tog'ri tortburchakning birinchi tomonini kiriting."),
+//   +prompt("Tog'ri tortburchakning ikkinchi tomonini kiriting."),
+//   +prompt("Tog'ri tortburchakning uchinchi tomonini kiriting."),
+//   +prompt("Tog'ri tortburchakning tortinchi tomonini kiriting.")
+// );
+
+// 44-masala
+// function ismFam(ism, familiya) {
+//     console.log(familiya + " " + ism);
+// }
+
+// ismFam(prompt("Ismingizni kiriting."), prompt("Familiyangizni kiriting."))
+
+// 45-masala
+//chunmadim
+
+// 46-masala
